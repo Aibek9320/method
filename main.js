1e9fc691eb6be070e50f24f36495bde2a2c299ec
Aibek9320 <123246995+Aibek9320@users.noreply.github.com>
2023-02-21 17:57:21 +0600

    till 10

--- main.js
+++ main.js
@@ -1,36 +1,59 @@
-const task4 = (arr) => {
-    return arr.map(el => el.length)
+const task1 = (one, two) => {
+    return one.map((el,ind) => el+two[ind])
 }
-console.log(task4(['Термос', 'Ураган', 'Земля', 'Комбо', 'Корень']))
+console.log(task1([2,4],[1,3]))
 
-const task5 = (num) => {
-    return num.map(el => `<div>${el}</div>`)
-}
-console.log(task5([1, 2, 3, 4, 5]))
+const task2 = [1,2,3,4,5]
+console.log(task2.map(el => {
+    return el*2
+}))
 
 
-
-const task6 = (mmm) => {
-    return mmm.filter(el => el.length % 2 === 0 )
-}
-console.log(task6(['ab', 'abc', 'abcd']))
-
-const task7 = (ide) => {
+const task3 = (ide) => {
     return ide.map(el => el.id)
 }
-console.log(task7([{id: 1},{id: 2},{id: 3},{id: 4}]))
+console.log(task3([{id: 1},{id: 2},{id: 3},{id: 4}]))
 
-const task8 = (nam) => {
-    return nam.filter( el => el*2 %10===0)
-} 
-console.log(task8([1,2,3,7,10,5]))
+// const task4 = (arr) => {
+//     return arr.map(el => el.length)
+// }
+// console.log(task4(['Термос', 'Ураган', 'Земля', 'Комбо', 'Корень']))
 
-const task9 = (boo) => {
+const task4 = (num) => {
+    return num.map(el => `<div>${el}</div>`)
+}
+console.log(task4([1, 2]))
+
+const task5 = (five) => {
+    return five.filter(el => el.length % 2 === 0)
+}
+console.log(task5(['ab','abc','abcd']))
+
+// const task6 = (nam) => {
+//     return nam.map( el => el * 2)
+// } 
+// console.log(task6([1,2,3,7,10,5]))
+
+
+const task7 = (boo) => {
     return boo.filter (el => el ===true)
 }
-console.log(task9([false,true]))
+console.log(task7([true, false, true, true, true]))
 
-const task10 = (und) => {
+const task8 = (und) => {
     return und.filter (el => el !==undefined)
 }
-console.log(task10([undefined, true, undefined]))
+console.log(task8([undefined, true, undefined]))
+
+const task9 = [2, 11, 5]
+
+console.log(task9.reduce((acc, rec, idx, arr) => {
+    return acc + rec
+} ));
+
+const task10 = [true,true,true,true ]
+
+console.log(task10.reduce((acc, rec, idx, arr)=> {
+    return
+}))
+
